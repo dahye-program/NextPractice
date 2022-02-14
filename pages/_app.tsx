a60b6e6340b6f5e2f5dfe8043c0384c287f3aebc
dahye-program <dahye0729@sch.ac.kr>
2022-02-15 02:02:04 +0900

    Add : getStatic props 받아오기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,7 @@ import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      This is _app
-      <Component {...pageProps} />
-    </>
+    <Component {...pageProps} />
   )
 }
 
